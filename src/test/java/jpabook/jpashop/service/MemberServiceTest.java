c536103cdc648ff3e2066a5afbece6ef11c80c83
Wonssun <heidencena1@gmail.com>
2020-07-22 22:12:57 +0900

    회원 도메인 개발

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,59 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Autowired EntityManager em;
+
+    @Test
+    @Rollback(false)
+    public void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("Yoo");
+
+        //when
+        Long savedId = memberService.join(member);
+
+        //then
+        assertEquals(member, memberRepository.findOne(savedId));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void 중복_회원_예외() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("yoo");
+
+        Member member2 = new Member();
+        member2.setName("yoo");
+
+        //when
+        memberService.join(member1);
+        memberService.join(member2);
+
+        //then
+        fail("이미 존재하는 회원입니다.");
+    }
+
+
+}
