7df2eb2920258d13a6f179844803c041f7dbd6b3
Wonssun <heidencena1@gmail.com>
2020-07-25 20:15:08 +0900

    주문 서비스(주문, 주문 취소)

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,13 +1,16 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 타 클래스에서 객체 생성 못하게 처리.
 public class OrderItem {
 
     @Id @GeneratedValue
