05ef7612a7a8978c84aed9367b300ef86b93e317
Wonssun <heidencena1@gmail.com>
2020-07-27 02:08:03 +0900

    css

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -42,7 +42,7 @@ public class MemberController {
         return "redirect:/";
     }
     
-    @GetMapping
+    @GetMapping("/members")
     public String list(Model model) {
         List<Member> members = memberService.findMembers();
         model.addAttribute("members", members);
